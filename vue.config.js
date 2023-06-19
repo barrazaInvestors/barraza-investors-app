60072f680ea29955c414327deed6d9d2f512d645
barrazaInvestors <somosfaifa@gmail.com>
2023-06-19 00:03:06 -0500

    volviendo a iconos medianos

--- vue.config.js
+++ vue.config.js
@@ -16,17 +16,24 @@ module.exports = defineConfig({
     msTileColor: "#37b7e3", // Color de la barra de tareas en dispositivos Windows
     appleMobileWebAppCapable: "yes", // Habilitar el modo pantalla completa en dispositivos iOS
     appleMobileWebAppStatusBarStyle: "#37b7e3", // Color de la barra de estado en dispositivos iOS
-    iconPaths: {
-      favicon134: "./img/icons/logo-icon-pwa-white.png",
-      appleTouchIcon: "./img/icons/logo-icon-pwa.png",
-      maskIcon: "./img/icons/logo-icon-pwa.png",
-      msTileImage: "./img/icons/logo-icon-pwa.png",
-    },
     manifestOptions: {
       short_name: "Barraza Investors",
       start_url: "/", // URL de inicio de la aplicación
       display: "standalone", // Estilo de visualización de la aplicación
       background_color: "#37b7e3", // Color de fondo de la pantalla de carga inicial
+      icons: [
+        {
+          src: "./img/icons/logo-icon-pwa-white.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/logo-icon-pwa.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable any",
+        },
+      ],
     },
   },
 });

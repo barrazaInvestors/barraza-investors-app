dcc7f3227bd93b6c3932a2e18d8bdfa2b1b57f43
barrazaInvestors <somosfaifa@gmail.com>
2023-06-18 22:18:16 -0500

    actualizando logos de icon y de inicio de la pwa, ademas de short name de la app

--- vue.config.js
+++ vue.config.js
@@ -12,44 +12,19 @@ module.exports = defineConfig({
     },
   },
   pwa: {
-    name: "Barraza Investors",
     themeColor: "#37b7e3",
     msTileColor: "#37b7e3", // Color de la barra de tareas en dispositivos Windows
     appleMobileWebAppCapable: "yes", // Habilitar el modo pantalla completa en dispositivos iOS
     appleMobileWebAppStatusBarStyle: "#37b7e3", // Color de la barra de estado en dispositivos iOS
+    iconPaths: {
+      favicon32: "./img/icons/logo-icon-pwa-white.png",
+      favicon16: "./img/icons/logo-icon-pwa-white.png",
+      appleTouchIcon: "./img/icons/logo-icon-pwa.png",
+      maskIcon: "./img/icons/logo-icon-pwa.png",
+      msTileImage: "./img/icons/logo-icon-pwa.png",
+    },
     manifestOptions: {
-      icons: [
-        {
-          src: "./img/icons/logo-icon-pwa.png", // Ruta relativa al archivo de icono
-          sizes: "48x48",
-          type: "image/png",
-        },
-        {
-          src: "./img/icons/logo-icon-pwa.png",
-          sizes: "72x72",
-          type: "image/png",
-        },
-        {
-          src: "./img/icons/logo-icon-pwa.png",
-          sizes: "96x96",
-          type: "image/png",
-        },
-        {
-          src: "./img/icons/logo-icon-pwa.png",
-          sizes: "144x144",
-          type: "image/png",
-        },
-        {
-          src: "./img/icons/logo-icon-pwa.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "./img/icons/logo-icon-pwa.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
+      short_name: "Barraza Investors",
       start_url: "/", // URL de inicio de la aplicación
       display: "standalone", // Estilo de visualización de la aplicación
       background_color: "#37b7e3", // Color de fondo de la pantalla de carga inicial

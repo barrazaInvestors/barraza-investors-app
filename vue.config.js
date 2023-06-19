1e5b4912d9c225f524e2fba4483af92cbdb5eeb3
barrazaInvestors <somosfaifa@gmail.com>
2023-06-19 01:22:52 -0500

    dejando los iconoc iniciales

--- vue.config.js
+++ vue.config.js
@@ -23,8 +23,33 @@ module.exports = defineConfig({
       background_color: "#37b7e3", // Color de fondo de la pantalla de carga inicial
       icons: [
         {
+          src: "./img/icons/logo-icon-pwa.png", // Ruta relativa al archivo de icono
+          sizes: "48x48",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/logo-icon-pwa.png",
+          sizes: "72x72",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/logo-icon-pwa.png",
+          sizes: "96x96",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/logo-icon-pwa.png",
+          sizes: "144x144",
+          type: "image/png",
+        },
+        {
           src: "./img/icons/logo-icon-pwa.png",
           sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/logo-icon-pwa.png",
+          sizes: "512x512",
           type: "image/png",
         },
       ],
